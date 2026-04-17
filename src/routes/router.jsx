6d170523c0188a4c106049e0fa1061babc63d8d7
Thiago Gabriel <thiagogab.20@gmail.com>
2026-04-17 18:28:01 -0300

    feat: criação da tela de cadastro

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 
 import LandingPage from '../landing/LandingPage'
 import Login from '../features/auth/pages/Login'
+import Register from '../features/auth/pages/Register'
 
 function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
         </Routes>
     )
 }
